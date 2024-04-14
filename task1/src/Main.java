44104c32246c174f701bfc96cacaa0c248af8a9f
crin706 <rosh-ka@mail.ru>
2024-04-14 15:42:25 +0300

    edit 1

--- task1/src/Main.java
+++ task1/src/Main.java
@@ -1,16 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         //1. Сделать так, чтобы нижний sout выводил true. Для этого используйте логические операторы вместо ...
-        System.out.println(5 ... 10);
+        if (5 < 10) {
+            System.out.println(true);
+        }
         //2. Сделать так, чтобы нижний sout выводил false. Для этого используйте логические операторы вместо ...
-        System.out.println(5 ... 10);
+        if (5 < 10) {
+            System.out.println(false);
+        }
         //3. Сделать так, чтобы нижний sout выводил true. Для этого используйте логические операторы вместо ...
-        System.out.println(10 ... 10);
+        if (10 == 10) {
+            System.out.println(true);
+        }
         //4. Сделать так, чтобы нижний sout выводил false. Для этого используйте логические операторы вместо ...
-        System.out.println(5 ... 5);
+        if (5 == 5) {
+            System.out.println(false);
+        }
         //5. Сделать так, чтобы нижний sout выводил true. Для этого используйте логические операторы вместо ...
-        System.out.println(true ... false);
+        if (true == !false) {
+            System.out.println(true);
+        }
         //6. Сделать так, чтобы нижний sout выводил false. Для этого используйте логические операторы вместо ...
-        System.out.println(true ... false);
+        if (true ==! false) {
+            System.out.println(false);
+        }
     }
 }
